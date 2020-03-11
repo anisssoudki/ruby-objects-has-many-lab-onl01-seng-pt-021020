c04a5afe1914229a5038dad8a44c4c5d5c04a482
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-11 22:10:30 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -33,7 +33,7 @@ class Author
 
   def add_post_by_title(post_title)
     post = Post.new(post_title)
-    @posts << post(add_post)
+    @posts << post
     post.author = self
     @@post_count += 1
   end
