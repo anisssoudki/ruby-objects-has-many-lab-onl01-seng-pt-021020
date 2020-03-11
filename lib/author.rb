a3adae26728cdac2440f283aecb65513c7c07cd5
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-11 21:56:55 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,48 @@
+# Author
+#   #new
+#     is initialized with a name (FAILED - 4)
+#   #name
+#     has an attr_accessor for name (FAILED - 5)
+#   #posts
+#     has many posts (FAILED - 6)
+#   #add_post
+#     takes in an argument of a post and associates that post with the author by telling the post that it belongs to that author (FAILED - 7)
+#   #add_post_by_title
+#     takes in an argument of a post title, creates a new post with it and associates the post and author (FAILED - 8)
+#   .post_count
+#     is a class method that returns the total number of posts associated to all existing authors (FAILED - 9)
+require "pry"
+class Author 
+attr_accessor :name, :posts
+@@post_count =  0
+def initialize(name)
+@name = name 
+@posts = []
+end
+
+# def posts 
+#   Post.all.select do |n|
+#     posts.author == self 
+    
+#   end
+# end
+  
+
+def add_post(postarg)
+ newpost = Post.new(postarg)
+  @posts << newpost
+ 
+  @@post_count += 1 
+end
+def add_post_by_title(title)
+  title = Post.new(title)
+  @posts << add_post(title)
+  posts.author = self 
+  @@post_count +=1
+end
+def self.post_count
+  
+  @@post_count
+end
+end
+
